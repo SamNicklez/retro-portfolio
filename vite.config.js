3577e9552ff9d117250537809e5fac6fa4785940
SamNicklez <snicklaus@uiowa.edu>
2024-04-22 15:47:08 -0500

    added deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://samnicklez.github.io/retro-portfolio/',
+  publicPath: '/retro-portfolio/',
+  baseUrl: '/retro-portfolio/',
   plugins: [
     vue({
       template: {
@@ -14,5 +18,10 @@ export default defineConfig({
   ],
   optimizeDeps: {
     exclude: ['@a2000/icons']
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
