1bb9f06b0d1eb73d4ced0131aec4595ca7f8791c
SamNicklez <snicklaus@uiowa.edu>
2024-05-20 12:38:59 -0500

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/SamNicklez/retro-portfolio.git master:gh-pages
+git push -f https://github.com/SamNicklez/windows2000-portfolio-website.git master:gh-pages
 
 cd -
